fd401012c33c63a3e8f4e1f002377dd0d89af62d
Nashet <Nashet@users.noreply.github.com>
2023-09-30 23:02:21 +0200

    Add save load panel

---
+++ Assets/ECSFileWork/Scripts/Views/SaveLoadPanelView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Nashet.ECSFileWork.Views
+{
+	public delegate void DropdownChangedDelegate(int value);
+	public class SaveLoadPanelView : MonoBehaviour
+	{
+		public event ButtonClickedDelegate OnSaveClicked;
+		public event ButtonClickedDelegate OnLoadClicked;
+		public event DropdownChangedDelegate OnDropdownValueChanged;
+		public void OnSaveClickedHandler()
+		{
+			OnSaveClicked?.Invoke();
+		}
+
+		public void OnLoadClickedHandler()
+		{
+			OnLoadClicked?.Invoke();
+		}
+
+		public void OnDropdownValueChangedHandler(int value)
+		{
+			OnDropdownValueChanged?.Invoke(value);
+		}
+	}
+}
